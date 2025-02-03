a8f35bd57d9202981c010fed1f18cbcb7eed8548
Marat Abrarov <abrarov@gmail.com>
2025-02-03 11:33:47 +0300

    Word counter.

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module hello-world
+
+go 1.23.2
+
+require golang.org/x/tour v0.1.0
--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"golang.org/x/tour/wc"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
-func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
+func WordCount(s string) map[string]int {
+	dict := make(map[string]int)
+	last := len(s) - 1
+	var word string
+	for i, c := range s {
+		boundary := i == last
+		switch c {
+		case ' ', '\t', '\r', '\n':
+			boundary = true
+		default:
+			word += string(c)
+		}
+		if boundary && len(word) > 0 {
+			dict[word] += 1
+			word = ""
+		}
 	}
+	return dict
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func main() {
+	wc.Test(WordCount)
+}
