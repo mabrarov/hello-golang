196da0ad768bde5cddd894f74af75ae8b1cfa1a0
Marat Abrarov <abrarov@gmail.com>
2025-02-08 00:28:22 +0300

    Readers lesson.

--- main.go
+++ main.go
@@ -2,40 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
+	"io"
+	"strings"
 )
 
-var m = map[int]string{
-	0:  "zero",
-	1:  "one",
-	2:  "two",
-	3:  "three",
-	4:  "four",
-	5:  "five",
-	6:  "six",
-	7:  "seven",
-	8:  "eight",
-	9:  "nine",
-	10: "ten",
-}
-
 func main() {
-	fmt.Print("Enter integer number within [0, 10]: ")
-	var text string
-	_, err := fmt.Scanln(&text)
-	if err != nil {
-		fmt.Printf("Failed to get input: %v\n", err)
-		os.Exit(1)
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
 	}
-	num, err := strconv.Atoi(text)
-	if err != nil {
-		fmt.Printf("Failed to convert input to integer number: %v\n", err)
-		os.Exit(1)
-	}
-	if num < 0 || num >= len(m) {
-		fmt.Printf("Provided number is out of range: %v\n", num)
-		os.Exit(1)
-	}
-	fmt.Printf("You entered: %v\n", m[num])
 }
