90a71fa9913ef2bf665a1ba0bc61bf99db4051ce
Marat Abrarov <abrarov@gmail.com>
2025-02-08 00:32:35 +0300

    For loop transformation.

--- main.go
+++ main.go
@@ -10,12 +10,10 @@ func main() {
 	r := strings.NewReader("Hello, Reader!")
 
 	b := make([]byte, 8)
-	for {
-		n, err := r.Read(b)
+	for err := error(nil); err != io.EOF; {
+		n, e := r.Read(b)
+		err = e
 		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
 		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
-			break
-		}
 	}
 }
