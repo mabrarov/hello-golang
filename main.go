9bf2259e65bceaa6f5ed08a6b1689eafbb64c601
Marat Abrarov <abrarov@gmail.com>
2025-02-07 16:09:53 +0300

    Number to text.

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
-// fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
-	current, next := 0, 1
-	return func() int {
-		r := current
-		current, next = next, current+next
-		return r
-	}
+var m = map[int]string{
+	0:  "zero",
+	1:  "one",
+	2:  "two",
+	3:  "three",
+	4:  "four",
+	5:  "five",
+	6:  "six",
+	7:  "seven",
+	8:  "eight",
+	9:  "nine",
+	10: "ten",
 }
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+	fmt.Print("Enter integer number within [0, 10]: ")
+	var text string
+	_, err := fmt.Scanln(&text)
+	if err != nil {
+		fmt.Printf("Failed to get input: %v\n", err)
+		os.Exit(1)
 	}
+	num, err := strconv.Atoi(text)
+	if err != nil {
+		fmt.Printf("Failed to convert input to integer number: %v\n", err)
+		os.Exit(1)
+	}
+	if num < 0 || num >= len(m) {
+		fmt.Printf("Provided number is out of range: %v\n", num)
+		os.Exit(1)
+	}
+	fmt.Printf("You entered: %v\n", m[num])
 }
