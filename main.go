1a3cbf9bcd5915d8772f7bb43e6c228dcd181d61
Marat Abrarov <abrarov@gmail.com>
2025-02-08 00:33:23 +0300

    Revert "For loop transformation."
    This reverts commit 90a71fa9913ef2bf665a1ba0bc61bf99db4051ce.

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ func main() {
 	r := strings.NewReader("Hello, Reader!")
 
 	b := make([]byte, 8)
-	for err := error(nil); err != io.EOF; {
-		n, e := r.Read(b)
-		err = e
+	for {
+		n, err := r.Read(b)
 		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
 		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
 	}
 }
