7d2f0c51799733fbc4a74e4eb36263848acdfc7b
Marat Abrarov <abrarov@gmail.com>
2025-02-08 13:59:25 +0300

    Channels.

--- main.go
+++ main.go
@@ -2,73 +2,21 @@ package main
 
 import "fmt"
 
-// List represents a singly-linked list that holds
-// values of any type.
-type List[T any] struct {
-	next *List[T]
-	val  T
-}
-
-func forEach[T any](list *List[T], f func(index int, item *T)) {
-	for i, item := 0, list; item != nil; i++ {
-		f(i, &item.val)
-		item = item.next
-		if item == list {
-			break
-		}
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
 	}
-}
-
-func addAfter[T any](list *List[T], val T) *List[T] {
-	item := &List[T]{val: val}
-	if list == nil {
-		item.next = item
-	} else {
-		list.next, item.next = item, list.next
-	}
-	return item
-}
-
-func removeAfter[T any](list *List[T]) *List[T] {
-	if list == nil {
-		return nil
-	}
-	if list == list.next || list.next == nil {
-		list.next = nil
-		return nil
-	}
-	list.next, list.next.next = list.next.next, nil
-	return list
-}
-
-func printList[T any](list *List[T]) {
-	forEach(list, func(index int, item *T) {
-		fmt.Printf("item[%d]=%v\n", index, *item)
-	})
+	c <- sum // send sum to c
 }
 
 func main() {
-	var list *List[int]
-	for i, last := 0, list; i < 10; i++ {
-		last = addAfter(last, i)
-		if list == nil {
-			list = last
-		}
-	}
-	fmt.Println("Created list:")
-	printList(list)
-	forEach(list, func(index int, item *int) {
-		*item += index
-	})
-	fmt.Println("Processed list:")
-	printList(list)
-	list = removeAfter(list)
-	list = removeAfter(list)
-	fmt.Println("Reduced list:")
-	printList(list)
-	for list != nil {
-		list = removeAfter(list)
-	}
-	fmt.Println("Empty list:")
-	printList(list)
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+
+	r := <-c + <-c
+	fmt.Println(r)
 }
