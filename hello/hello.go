bb585f28f55da8db00733497e770782fbd2249b4
Marat Abrarov <abrarov@gmail.com>
2025-02-09 22:37:14 +0300

    Return and handle an error.

--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"fmt"
-
 	"example.com/greetings"
+	"fmt"
+	"log"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.LUTC | log.Lshortfile | log.Lmsgprefix)
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("Gladys")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
