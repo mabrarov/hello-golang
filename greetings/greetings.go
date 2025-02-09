938844082b5da197f4887e956a783247f98e37ce
Marat Abrarov <abrarov@gmail.com>
2025-02-10 00:51:24 +0300

    Random greeting.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,7 +3,15 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
+
+// A slice of message formats.
+var formats = []string{
+	"Hi, %v. Welcome!",
+	"Great to see you, %v!",
+	"Hail, %v! Well met!",
+}
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
@@ -13,6 +21,14 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
