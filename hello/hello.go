ad30ab75bb5c28e5dcc78a58fcc24eb94a63b2e3
Marat Abrarov <marat.abrarov@travelline.ru>
2025-02-12 15:38:18 +0300

    Revert "A try to move tests into another Go module."
    This reverts commit 4cd062806334dc68cb0b6047c3a7b15953f1a75d.

--- hello/hello.go
+++ hello/hello.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/mabrarov/hello-golang/hello/internal/greetings"
+	"github.com/mabrarov/hello-golang/greetings"
 	"log"
 	"rsc.io/quote"
 )
@@ -14,7 +14,7 @@ func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.LUTC | log.Lshortfile | log.Lmsgprefix)
 
 	// Get a greeting message and print it.
-	messages, err := greetings.GenGreetings()
+	messages, err := greetings.Hellos("Gladys", "Samantha", "Darrin")
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
--- hello/internal/greetings/greetings.go
+++
@@ -1,9 +0,0 @@
-package greetings
-
-import (
-	"github.com/mabrarov/hello-golang/greetings"
-)
-
-func GenGreetings() ([]string, error) {
-	return greetings.Hellos("Gladys", "Samantha", "Darrin")
-}
